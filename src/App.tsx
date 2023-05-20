ee0b1fc228a99899ae8462e570b88fd27e032833
Maylon-tech <minatokiroisen77@gmail.com>
2023-05-20 20:18:49 +0900

    starting the app and some config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Container } from './styles/GlobalStyle'
+
+const App = () => {
+  return (
+    <Container>
+      <h1>Nemoto Dev</h1>
+      <h2>Welcome to React Portifolio feat React and Next developer</h2>
+    </Container>
+  )
+}
+
+export default App
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  margin: 0;
+  display: flex;
+  place-items: center;
+  flex-direction: column;
+  min-width: 720px;
+  min-height: 100vh;
+
+  > h1 {
+  font-size: 2.5em;
+  line-height: 1.1;
+}
+`
